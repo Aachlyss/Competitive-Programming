1a19c5899d0e30c1b08a3a91ceacad76ae8b87e8
Aachlyss <ivanangelov05@gmail.com>
2023-03-06 09:57:39 +0200

    First Commit

---
+++ ThreadExample/ThreadExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadExample
+{
+    class Program
+    {
+        static Random random = new Random();
+        static void CountBackwards()
+        {
+            int waitTime = random.Next(1, 10) * 1000;
+            Console.WriteLine($"Wait time: {waitTime}");
+            for (int i = 0; i >=0; i--)
+            {
+                Console.WriteLine(i);
+                System.Threading.Thread.Sleep(waitTime);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(CountBackwards);
+            Thread thread2 = new Thread(CountBackwards);
+            thread1.Start(); thread2.Start();
+        }
+    }
+}
