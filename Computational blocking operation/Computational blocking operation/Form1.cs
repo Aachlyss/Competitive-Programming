b84b4b05d95697b86bffa729b18d9f45535e2c2f
Aachlyss <ivanangelov05@gmail.com>
2023-03-13 09:59:53 +0200

    Commit

---
+++ Computational blocking operation/Computational blocking operation/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Computational_blocking_operation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int number = int.Parse(textBox1.Text);
+                MessageBox.Show("Резултат: " + Factorial(number));
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Моля въведете валидно цяло число.");
+            }
+        }
+
+        private BigInteger Factorial(BigInteger number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+    }
+}
