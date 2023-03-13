bcfedb09b2099ab928697bac5724c6fc0db56e99
Aachlyss <ivanangelov05@gmail.com>
2023-03-13 10:11:25 +0200

    Commit

---
+++ FileReadingBlocking/FileReadingBlocking/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileReadingBlocking
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.Filter = "Text Files (*.txt)|*.txt";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                string fileContent = System.IO.File.ReadAllText(fileName);
+                textBox1.Text = fileContent;
+            }
+        }
+    }
+}
